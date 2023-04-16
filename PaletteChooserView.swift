cb57e1ee37e8a30063e5238ac462c1481f16d5e1
Zeeshan Shakeel <zee.shakeel@gmail.com>
2023-04-16 21:43:54 +0500

    Lecture 12: PaletteEditor with an edit form added.

--- PaletteChooserView.swift
+++ PaletteChooserView.swift
@@ -43,6 +43,43 @@ struct PaletteChooserView: View {
         }
         .id(palette.id)
         .transition(rollTransition)
+//        .popover(isPresented: $editing){
+//            PaletteEditorView(palette: $store.palettes[chosenPaletteIndex])
+//        }
+        .popover(item: $paletteToEdit) { palette in
+            PaletteEditorView(palette: $store.palettes[palette])
+        }
+    }
+    
+    //@State private var editing = false
+    @State private var managing = false
+    @State private var paletteToEdit: Palette?
+    
+    var rollTransition: AnyTransition {
+        AnyTransition.asymmetric(
+            insertion: .offset(x: 0, y: emojiFontSize),
+            removal: .offset(x: 0, y: -emojiFontSize)
+        )
+    }
+    
+    @ViewBuilder
+    var contextMenu: some View {
+        AnimatedActionButton(title: "Edit", systemImage: "pencil") {
+            //editing = true
+            paletteToEdit = store.palette(at: chosenPaletteIndex)
+        }
+        AnimatedActionButton(title: "New", systemImage: "plus") {
+            store.insertPalette(named: "New", emojis: "", at: chosenPaletteIndex)
+            //editing = true
+            paletteToEdit = store.palette(at: chosenPaletteIndex)
+        }
+        AnimatedActionButton(title: "Delete", systemImage: "minus.circle") {
+            chosenPaletteIndex = store.removePalette(at: chosenPaletteIndex)
+        }
+        AnimatedActionButton(title: "Manager", systemImage: "slider.vertical.3") {
+            managing = true
+        }
+        gotoMenu
     }
     
     var gotoMenu: some View {
@@ -57,33 +94,6 @@ struct PaletteChooserView: View {
         } label: {
             Label("Go To", systemImage: "text.insert")
         }
-    }
-    
-    var rollTransition: AnyTransition {
-        AnyTransition.asymmetric(
-            insertion: .offset(x: 0, y: emojiFontSize),
-            removal: .offset(x: 0, y: -emojiFontSize)
-        )
-    }
-    
-    @ViewBuilder
-    var contextMenu: some View {
-        AnimatedActionButton(title: "Edit", systemImage: "pencil") {
-//            editing = true
-            //paletteToEdit = store.palette(at: chosenPaletteIndex)
-        }
-        AnimatedActionButton(title: "New", systemImage: "plus") {
-            store.insertPalette(named: "New", emojis: "", at: chosenPaletteIndex)
-//            editing = true
-            //paletteToEdit = store.palette(at: chosenPaletteIndex)
-        }
-        AnimatedActionButton(title: "Delete", systemImage: "minus.circle") {
-            chosenPaletteIndex = store.removePalette(at: chosenPaletteIndex)
-        }
-        AnimatedActionButton(title: "Manager", systemImage: "slider.vertical.3") {
-            //managing = true
-        }
-        gotoMenu
     }
 }
 
